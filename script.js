15fe0c9ec78a21cb05cbcd3fbb06c5d34455a543
keivin-r <129949532+keivin-r@users.noreply.github.com>
2023-10-15 16:20:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let userName;
+
+window.prompt("Enter our name nigga!");
+alert(`Wazzup Mah negga ${userName}!`);
+
+document.getElementById("myButton").onclick = function(){
+
+    userName = document.getElementById("myText").value;
+    alert(`Kung ang buwan ay bilog ${userName} gusto kita.`);
+    alert(`Hi ${userName} kamusta?`);
+    alert(`Sana okay kalang`);
+    alert(`I love you ${userName}`);
+    alert(`Joke lang HAHAH uto uto ampota`);
+    alert(`${userName} ano na bai?`);
+    alert(`Ina neto ni ${userName}`);
+    alert(`Review review na ${userName} kalako ba aayusin mona sa school?`);
+    alert(`Naneto`);
+    alert(`Inamo`);
+    alert(`gago`);
+    alert(`fuk u`);
+    alert(`Tarantado`);
+    alert(`HAHAHHAHAHAHAHAH`);
+    alert(`jk lang ${userName} baka mamaya soboen moto e`)
+    document.getElementById("myLabel").innerHTML = `Hello ${userName} Sakin ka nalang.`
+}
